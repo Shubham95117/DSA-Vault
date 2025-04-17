19a798ca84cd18ebae6b10cac2f0a76b6931c988
shubham <shubhamchavan71195@gmail.com>
2025-04-17 09:57:38 +0530

    started basics of language

---
+++ LearnTheBasics/1_Basics-language/input-output.js
@@ -0,0 +1,13 @@
+// Complete the function printNumber which takes an integer input from the user and prints it on the screen.
+
+class Solution {
+  printNumber(value) {
+    console.log(value);
+  }
+}
+
+//creating object
+
+const solution = new Solution();
+
+solution.printNumber(7);
