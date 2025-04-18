6a0cc1a86c64f1316cdf1d4fc1ba199bfd3cc05c
shubham <shubhamchavan71195@gmail.com>
2025-04-18 12:10:48 +0530

    ..

---
+++ LearnTheBasics/1_Basics-language/switch.js
@@ -0,0 +1,28 @@
+// Syntax;
+// javascript;
+switch (expression) {
+  case value1:
+    // Code to execute if expression === value1
+    break;
+  case value2:
+    // Code to execute if expression === value2
+    break;
+  default:
+  // Code to execute if no case matches
+}
+
+let fruit = "apple";
+
+switch (fruit) {
+  case "banana":
+    console.log("Bananas are yellow.");
+    break;
+  case "apple":
+    console.log("Apples are red or green.");
+    break;
+  case "orange":
+    console.log("Oranges are orange.");
+    break;
+  default:
+    console.log("Unknown fruit.");
+}
